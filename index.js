9e782ff21adcdc0775576b4f45aedef7e5c6a714
Francis Odhiambo2 <francisodhiambo2@gmail.com>
2022-06-22 21:23:44 +0300

    Array map method

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+// const tutorials = [
+//   'what does the this keyword mean?',
+//   'What is the Constructor OO pattern?',
+//   'implementing Blockchain Web API',
+//   'The Test Driven Development Workflow',
+//   'What is NaN and how Can we Check for it',
+//   'What is the difference between stopPropagation and preventDefault?',
+//   'Immutable State and Pure Functions',
+//   'what is the difference between == and ===?',
+//   'what is the difference between event capturing and bubbling?',
+//   'what is JSONP?'
+// ];
+
+// const titleCased = () => {
+//   return tutorials
+// }
+
 const tutorials = [
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
@@ -12,5 +29,14 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  const tutorialList = tutorials.map((sentence) => {
+    const words = sentence.split(" ");
+    const wordTitle = words.map((word) =>
+      word.charAt(0).toUpperCase() + word.slice(1));
+    const result = wordTitle.join(" ");
+    return result;
+  })
+  return tutorialList;
 }
+
+//Map method
